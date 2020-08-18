6a94d23514421f88cf5ea22db2d26ef041f69184
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-18 19:40:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+function mapToNegativize(numbers){
+  return numbers.map(negativize);
+}
+function negativize(number){
+  return -number;
+}
+
+//mapToNoChange
+function mapToNoChange(array){
+  const newArray=[];
+   array.forEach(element =>newArray.push(element));
+   return newArray;
+}
+//mapToDouble
+function mapToDouble(array){
+  const newArray=[];
+  array.forEach(element =>newArray.push(element*2));
+  return newArray
+}
